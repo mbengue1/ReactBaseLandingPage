cb65ba5096ff0633ef9a4094baeb3582805543f7
Mouhamed Mbengue <Mouhamed23Mbengue@gmail.com>
2025-09-01 16:50:59 -0400

    Fix deployment: Add assets/index.js back to tracking

---
+++ src/assets/index.js
@@ -0,0 +1,46 @@
+
+
+
+//IMPORTS
+
+
+//Imports
+import { FaGithub } from "react-icons/fa";
+import { MdNightlight } from "react-icons/md";
+import { IoIosSunny } from "react-icons/io";
+import { FaLinkedin } from "react-icons/fa";
+import { IoMail } from "react-icons/io5";
+import HeadShot from './headShotMbengue.png';
+import MLThumb from './mlmodelthumb.webp';
+import SWEBlogAppThumb from './BlogAppThumb.webp';
+import BlogSlides from './BlogAppDisplays.pdf';
+import Resume from './Resume_August26_SWE2.pdf';
+import RocketThumb from './RocketThumb.webp';
+import ProjectNeptuneImage from './ProjectNeptune_image.png';
+import JobApplicationLoggerImage from './JobApplicatoinLogger_image.png';
+import IncidentResponseImage from './incidintreponse_imgag.png';
+import NovaThumb from './novathumb.png';
+
+
+//Temp Img
+import tempImg from './workinprogress.jpg';
+
+/**EXPORTS**/
+export {
+  HeadShot,
+  FaGithub,
+  MdNightlight,
+  IoIosSunny,
+  FaLinkedin,
+  IoMail,
+  tempImg,
+  MLThumb,
+  SWEBlogAppThumb,
+  BlogSlides,
+  Resume,
+  RocketThumb,
+  ProjectNeptuneImage,
+  JobApplicationLoggerImage,
+  IncidentResponseImage,
+  NovaThumb,
+};
